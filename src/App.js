ba50e75ff1380bbcb46ed18f473b3a4c7cf92ba2
Joseph <ejosephvito@gmail.com>
2019-10-04 13:46:56 -0400

    fixed bugs and added highscore tracking

--- src/App.js
+++ src/App.js
@@ -8,14 +8,24 @@ import "./App.css";
 class App extends Component {
   state = {
     characters,
+    mutableCharacters: characters,
     clicked: [],
-    points: 0
+    points: 0,
+    highscore: 0
   };
 
-  handleClick = name => {
-    const scramble = () => {};
+  handleClick = (name) => {
+    const scramble = () => {
+      this.setState({ mutableCharacters: this.state.mutableCharacters.sort(() => Math.random() - 0.5) });
 
+      // for (let i = array.length - 1; i > 0; i--) {
+      //   let j = Math.floor(Math.random() * (i + 1));
+      //   [array[i], array[j]] = [array[j], array[i]];
+      //   };
+      // }
+    };
     const restart = () => {
+      if (this.state.points > this.state.highscore) this.setState({ highscore: this.state.points });
       this.setState({ clicked: [], points: 0 });
       scramble();
     };
@@ -37,13 +47,9 @@ class App extends Component {
   render = () => {
     return (
       <Wrapper>
-        <Title score={this.state.score}></Title>
-        {this.state.characters.map(character => (
-          <CharacterCard
-            name={character.name}
-            image={character.image}
-            handleClick={this.handleClick}
-          />
+        <Title score={this.state.points}></Title>
+        {this.state.mutableCharacters.map((character) => (
+          <CharacterCard name={character.name} image={character.image} handleClick={this.handleClick} />
         ))}
       </Wrapper>
     );
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./style.css";
 
-function Title(props) {
+const Title = (props) => {
+  console.log(props);
+
   return <h1 className="title">Score: {props.score}</h1>;
-}
+};
 
 export default Title;
